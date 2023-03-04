057ff2d530b77a68c849bf128bbbf5bf57af438d
Jamshid K <jamshid.k@zeeve.io>
2023-03-04 16:41:06 +0530

    logger service added

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	mongoURL = "mongodb://localhost:27017"
+)
+
+var client *mongo.Client
+
+type AppConfig struct {
+	Models data.Models
+}
+
+func main() {
+	mongoClient, err := connectToMongo()
+
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	//create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	//connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+
+	//in order to disconnect need to create context
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	//close connection
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	if err != nil {
+		log.Println("Error connecting to Mongo:", err)
+		return nil, err
+	}
+
+	return c, nil
+}
